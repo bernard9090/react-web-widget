2787e8cc77739d255018fc3e309e5dd8a107c11f
Archibold Bernard <bernard.baidoo@rancard.com>
2021-03-17 14:25:16 +0000

    yarn old flow

--- webpack.config.js
+++ webpack.config.js
@@ -10,7 +10,7 @@ module.exports = {
     entry: './src/index.js',
     output: {
         path: path.resolve( __dirname, 'dist' ),
-        filename: 'sdp-ds-widget.js',
+        filename: 'sdp-ds-widget-sdp3.js',
         publicPath: '/',
     },
     devServer: {
